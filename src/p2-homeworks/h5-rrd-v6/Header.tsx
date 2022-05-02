ec88300ab037c5266d2aaedce5ac50c2935f2b05
IlyaGab <ilyagab1994@gmail.com>
2022-05-02 21:41:29 +0300

    Add task 05

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import {PATH} from './Pages';
+import c from './Header.module.css'
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    return (
+        <div className={c.navParent}> Navigation
+            <div className={c.navChild}>
+                <NavLink className={c.navLink} to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
+                <NavLink className={c.navLink} to={PATH.JUNIOR}>Junior</NavLink>
+                <NavLink className={c.navLink} to={PATH.JUNIOR_PLUS}>Junior-plus</NavLink>
+            </div>
         </div>
     )
 }
